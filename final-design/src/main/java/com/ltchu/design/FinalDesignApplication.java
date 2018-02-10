29c05c7abd6288b59aee7e1c84f533205123e9c2
zxguan <zxguan@zxguan-PC>
2018-02-10 23:06:08 +0800

    提交

---
+++ final-design/src/main/java/com/ltchu/design/FinalDesignApplication.java
@@ -0,0 +1,12 @@
+package com.ltchu.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalDesignApplication.class, args);
+	}
+}
